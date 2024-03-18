42ca6636c311490a544fd07110db458c84f06859
Henrique Coelho <riquecbeltrao@hotmail.com>
2024-03-18 07:56:54 -0300

    organizando pastas

--- analisa.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-x=$(sed "s/BRA/BR/g" $1 | grep -c "BR");
-
-echo "O arquivo $1 contém $x linhas com a sigla BRA"
--- testec.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-int main() {
-   printf("oie");
-   return 0;
-}
